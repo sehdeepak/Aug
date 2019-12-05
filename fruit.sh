d94520f3afb0c88357b206bf9f43fa1a2c5424e1
Deepak V N <deepak_vn@blrjpt3435d.ad.infosys.com>
2019-12-05 21:41:12 +0530

    pushed

--- fruit.sh
+++ fruit.sh
@@ -1,9 +1,9 @@
-hi
-fruit
-added
-new
-hello
-asd
-wer
-man
-der
+#!/bin/bash
+input=$1
+if [ "$input" == "apple" ]; then
+echo "this is fruit given fruit name is" $input
+elif [ "$input" == "orange" ]; then
+echo "this is fruit given fruit name is" $input
+else
+echo "bye i dont know you"
+fi
